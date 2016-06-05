d895f539b6999c1278fdcd723b31dcd0aefd87ee
Art <a.molcanovas@gmail.com>
2016-06-05 23:27:16 +0100

    more travis

--- tests/Misc/ClumsinessTest.php
+++ tests/Misc/ClumsinessTest.php
@@ -1,8 +1,8 @@
 <?php
     /**
- * Copyright (c) 2016 Alorel, https://github.com/Alorel
- * Licenced under MIT: https://github.com/Alorel/dropbox-v2-php/blob/master/LICENSE
- */
+     * Copyright (c) 2016 Alorel, https://github.com/Alorel
+     * Licenced under MIT: https://github.com/Alorel/dropbox-v2-php/blob/master/LICENSE
+     */
 
     namespace Alorel\Dropbox\ClumsinessPrevention;
 
@@ -15,68 +15,70 @@
     use Alorel\Dropbox\Util;
     use ReflectionClass as RC;
 
-    class ClumsinessTest extends \PHPUnit_Framework_TestCase {
+    if (1 == getenv('TRAVISCI')) {
+        class ClumsinessTest extends \PHPUnit_Framework_TestCase {
 
-        private static $BASE_NAMESPACE;
+            private static $BASE_NAMESPACE;
 
-        static function setUpBeforeClass() {
-            self::$BASE_NAMESPACE = (new RC(Util::class))->getNamespaceName();
-        }
-
-        private function abstractionSubclass(string $baseClass, string $testClass, ...$constructorArgs) {
-            $rc = new RC($testClass);
-
-            if ($rc->isInstantiable()) {
-                $this->assertInstanceOf($baseClass, $rc->newInstanceArgs($constructorArgs));
-            } else {
-                $parents = [$testClass];
-                while ($rc = $rc->getParentClass()) {
-                    $parents[] = $rc->getName();
-                }
-                $this->assertContains($baseClass, $parents);
+            static function setUpBeforeClass() {
+                self::$BASE_NAMESPACE = (new RC(Util::class))->getNamespaceName();
             }
-        }
 
-        /** @dataProvider providerOptionBuilder */
-        function testOptionBuilder(string $class) {
-            $this->abstractionSubclass(Options::class, $class);
-        }
+            private function abstractionSubclass(string $baseClass, string $testClass, ...$constructorArgs) {
+                $rc = new RC($testClass);
 
-        /** @dataProvider providerFilePaths */
-        function testFilePaths(string $class) {
-            $rf = new RC($class);
-            $trim = trim(str_replace(self::$BASE_NAMESPACE, '', $rf->getNamespaceName()), '\\');
-            $ns = 'src\\' . $trim . ($trim ? '\\' : '') . $rf->getShortName() . '.php';
+                if ($rc->isInstantiable()) {
+                    $this->assertInstanceOf($baseClass, $rc->newInstanceArgs($constructorArgs));
+                } else {
+                    $parents = [$testClass];
+                    while ($rc = $rc->getParentClass()) {
+                        $parents[] = $rc->getName();
+                    }
+                    $this->assertContains($baseClass, $parents);
+                }
+            }
 
-            $this->assertNotFalse(stripos($rf->getFileName(), $ns));
-        }
+            /** @dataProvider providerOptionBuilder */
+            function testOptionBuilder(string $class) {
+                $this->abstractionSubclass(Options::class, $class);
+            }
 
-        function providerFilePaths() {
-            return TestUtil::allClassesInClassDirectory(Util::class, true);
-        }
+            /** @dataProvider providerFilePaths */
+            function testFilePaths(string $class) {
+                $rf = new RC($class);
+                $trim = trim(str_replace(self::$BASE_NAMESPACE, '', $rf->getNamespaceName()), '\\');
+                $ns = 'src\\' . $trim . ($trim ? '\\' : '') . $rf->getShortName() . '.php';
 
-        function providerOptionBuilder() {
-            return TestUtil::allClassesInClassDirectory(GetMetadataOptions::class, true);
-        }
+                $this->assertNotFalse(stripos($rf->getFileName(), $ns));
+            }
 
-        /** @dataProvider providerOptionMixins */
-        function testOptionMixins(string $mixin) {
-            $this->assertTrue((new RC($mixin))->isTrait());
-        }
+            function providerFilePaths() {
+                return TestUtil::allClassesInClassDirectory(Util::class, true);
+            }
 
-        function providerOptionMixins() {
-            return TestUtil::allClassesInClassDirectory(AutoRenameTrait::class, true);
-        }
+            function providerOptionBuilder() {
+                return TestUtil::allClassesInClassDirectory(GetMetadataOptions::class, true);
+            }
 
-        /** @dataProvider providerOperationKind */
-        function testOperationKind(string $class) {
-            $this->abstractionSubclass(AbstractOperation::class, $class);
-        }
+            /** @dataProvider providerOptionMixins */
+            function testOptionMixins(string $mixin) {
+                $this->assertTrue((new RC($mixin))->isTrait());
+            }
 
-        function providerOperationKind() {
-            foreach ([RPCOperation::class, AbstractOperation::class] as $srcClass) {
-                foreach (TestUtil::allClassesInClassDirectory($srcClass, true) as $k => $v) {
-                    yield $k => $v;
+            function providerOptionMixins() {
+                return TestUtil::allClassesInClassDirectory(AutoRenameTrait::class, true);
+            }
+
+            /** @dataProvider providerOperationKind */
+            function testOperationKind(string $class) {
+                $this->abstractionSubclass(AbstractOperation::class, $class);
+            }
+
+            function providerOperationKind() {
+                foreach ([RPCOperation::class, AbstractOperation::class] as $srcClass) {
+                    foreach (TestUtil::allClassesInClassDirectory($srcClass, true) as $k => $v) {
+                        yield $k => $v;
+                    }
                 }
             }
         }
